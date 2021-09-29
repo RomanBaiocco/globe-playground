00a73e88dec0df8e13caf6a0df4a9c2ac4eb83bc
Roman Baiocco <roman@karat.com>
2021-09-29 11:24:55 -0700

    Add globe

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 import ReloadPrompt from './components/ReloadPrompt'
+import Globe from 'react-globe.gl'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <>
       <ReloadPrompt/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      />
+    </>
   )
 }
 
